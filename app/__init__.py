62626de7436b7160626c2017476e03b20f949491
Alvin Sze <alvins44@nycstudents.net>
2025-10-28 10:39:31 -0400

    python app opens to homepage

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,19 @@
+# Geodes
+# Softdev 2025
+# p00
 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def disp_homepage():
+    return render_template('homepage.html')
+
+@app.route("/login")
+def disp_login():
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
