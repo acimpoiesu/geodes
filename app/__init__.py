05c83a267269404105157dcabb7b154e7a6823e4
shafin kazi <shafinkazi5@gmail.com>
2025-10-30 18:48:22 -0400

    fixed what we started in class (sqlite dummy)

--- app/__init__.py
+++ app/__init__.py
@@ -8,16 +8,18 @@ import sqlite3
 
 
 app = Flask(__name__)
-# DB_FILE = "blog.db"
-# db = sqlite3.connect(DB_FILE)
-# c = db.cursor()
-#
-#
-# initusertable = "CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, blog_title TEXT, session_key TEXT, login_token TEXT);"
-# c.execute(initusertable)
-# initposttable = "CREATE TABLE IF NOT EXISTS posts (owner TEXT, post_title TEXT, post_text TEXT, timestamp TEXT);"
-# c.execute(initposttable)
-# c.execute("INSERT INTO users VALUES ('hi', 'hello', 'wowie', 'salkdgs', 'askjhgdafhg');")
+DB_FILE = "blog.db"
+db = sqlite3.connect(DB_FILE)
+c = db.cursor()
+
+
+initusertable = "CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, blog_title TEXT, session_key TEXT, login_token TEXT);"
+c.execute(initusertable)
+initposttable = "CREATE TABLE IF NOT EXISTS posts (owner TEXT, post_title TEXT, post_text TEXT, timestamp TEXT);"
+c.execute(initposttable)
+c.execute("INSERT INTO users VALUES ('hi', 'hello', 'wowie', 'salkdgs', 'askjhgdafhg');")
+
+db.commit()
 
 @app.route("/")
 def disp_homepage():
@@ -27,10 +29,16 @@ def disp_homepage():
 def disp_login():
     return render_template('login.html')
 
-# @app.route("/setuser", methods = ["POST"])
-# def set_user():
-#     if c.execute("EXISTS (SELECT * FROM users WHERE username = '" + username + "');"):
-#         print("WOOHOO")
+@app.route("/setuser", methods = ["POST"])
+def set_user():
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    testing = c.execute("SELECT * FROM users WHERE username = 'hi';")
+    for row in testing:
+        print("WOOHOO")
+        break
+    return render_template('homepage.html')
+
 @app.route("/logout")
 def disp_logout():
     return render_template('logout.html')
@@ -43,5 +51,4 @@ if __name__ == "__main__":
     app.debug = True
     app.run()
 
-# db.commit()
-# db.close()
+db.close()
