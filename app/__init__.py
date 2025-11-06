e542ccd82141e6136aa416613d688b0762a9a3ec
Alvin Sze <alvins44@nycstudents.net>
2025-11-06 11:47:50 -0500

    buggy code

--- app/__init__.py
+++ app/__init__.py
@@ -13,14 +13,15 @@ DB_FILE = "blog.db"
 db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 
-
-initusertable = "CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, blog_title TEXT, session_key TEXT, login_token TEXT);"
-c.execute(initusertable)
-initposttable = "CREATE TABLE IF NOT EXISTS posts (owner TEXT, post_title TEXT, post_text TEXT, timestamp TEXT);"
-c.execute(initposttable)
-c.execute("INSERT INTO users VALUES ('hi', 'hello', 'wowie', 'salkdgs', 'askjhgdafhg');")
+c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, blog_title TEXT, session_key TEXT, login_token TEXT);")
+c.execute("CREATE TABLE IF NOT EXISTS posts (owner TEXT, post_title TEXT, post_text TEXT, timestamp TEXT);")
+if not c.execute("SELECT * FROM users WHERE username = 'admin'") #Always returns false ????:
+    c.execute("INSERT INTO users VALUES ('admin', 'geodes1234', 'TEST BLOG', 'session_key', 'token')")
 
 db.commit()
+c.close()
+
+errormessage = "Query Failed"
 
 @app.route("/")
 def disp_homepage():
@@ -36,10 +37,12 @@ def set_user():
     c = db.cursor()
     username = request.form["username"]
     password = request.form["password"]
-    session["username"] = username
-    session["password"] = password
-    if c.execute(f"SELECT password FROM users WHERE username = '{username}'") == password:
-        currentuser = username
+    userinfo = c.execute(f"SELECT password FROM users WHERE username = '{username}';")
+    for password in userinfo:
+        if password == password:
+            session["username"] = username
+            session["password"] = password
+    c.close()
     return render_template('homepage.html')
 
 @app.route("/logout")
@@ -52,6 +55,10 @@ def disp_logout():
 def disp_create_account():
         return render_template("createaccount.html")
 
+@app.route("/error")
+def disp_error():
+    return render_template("error.html")
+
 @app.route("/creating", methods = ["POST"])
 def creating():
     db = sqlite3.connect(DB_FILE)
@@ -60,8 +67,15 @@ def creating():
         username = request.form["username"]
         password = request.form["password"]
         if password == request.form["confirm"]:
-            c.execute(f"INSERT INTO users VALUES ('{username}', '{password}', 'BLOG_TITLE', 'SESSION', 'TOKEN');")
-            return render_template('login.html')
+            if c.execute(f"SELECT * FROM users WHERE username = '{username}';") == None #Always returns false????:
+                errormessage = "Username Taken. Please Retry." #not working html doesnt display
+                return render_template('error.html')
+            else:
+                c.execute(f"INSERT INTO users VALUES ('{username}', '{password}', 'BLOG_TITLE', 'SESSION', 'TOKEN');") #Not sure if this actually adds
+                db.commit()
+                c.close()
+                return render_template('login.html')
+    return render_template("createaccount.html")
 
 @app.route("/profile")
 def disp_profile():
